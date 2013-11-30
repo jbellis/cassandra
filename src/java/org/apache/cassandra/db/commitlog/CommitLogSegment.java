de41c430f687723572793935e5fc2e735ee915b4
Jonathan Ellis <jbellis@apache.org>
2013-11-30 12:44:14 -0600

    simplify away flag to discard/delete

--- src/java/org/apache/cassandra/db/commitlog/CommitLogSegment.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLogSegment.java
@@ -305,11 +305,9 @@ public class CommitLogSegment
     /**
      * Completely discards a segment file by deleting it. (Potentially blocking operation)
      */
-    void discard(boolean deleteFile)
+    void delete()
     {
-        close();
-        if (deleteFile)
-            FileUtils.deleteWithConfirm(logFile);
+       FileUtils.deleteWithConfirm(logFile);
     }
 
     /**
--- src/java/org/apache/cassandra/db/commitlog/CommitLogSegmentManager.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLogSegmentManager.java
@@ -385,7 +385,9 @@ public class CommitLogSegmentManager
         {
             public CommitLogSegment call()
             {
-                segment.discard(deleteFile);
+                segment.close();
+                if (deleteFile)
+                    segment.delete();
                 return null;
             }
         });
