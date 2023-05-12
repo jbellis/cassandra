e48ed89bfedc2a4b79cfbd4e00e701b03a327e28
David Capwell <dcapwell@apache.org>
2023-05-12 14:07:04 -0700

    remove ProtocolVersion from toCQLLiteral

--- src/java/org/apache/cassandra/cql3/functions/masking/ColumnMask.java
+++ src/java/org/apache/cassandra/cql3/functions/masking/ColumnMask.java
@@ -173,7 +173,7 @@ public abstract class ColumnMask
         {
             CQL3Type type = types.get(i).asCQL3Type();
             ByteBuffer value = partialArgumentValues[i];
-            arguments.add(type.toCQLLiteral(value, ProtocolVersion.CURRENT));
+            arguments.add(type.toCQLLiteral(value));
         }
         return format("%s(%s)", function.name(), StringUtils.join(arguments, ", "));
     }
