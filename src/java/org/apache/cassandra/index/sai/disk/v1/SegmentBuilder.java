85c8527956fda227a27b8ea413a50180011c51a8
Jonathan Ellis <jbellis@datastax.com>
2023-08-23 06:21:49 -0700

    trying to hack in concurrent index builds

--- src/java/org/apache/cassandra/index/sai/disk/v1/SegmentBuilder.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/SegmentBuilder.java
@@ -20,6 +20,8 @@ package org.apache.cassandra.index.sai.disk.v1;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import javax.annotation.concurrent.NotThreadSafe;
 
@@ -27,6 +29,8 @@ import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.cassandra.concurrent.DebuggableThreadPoolExecutor;
+import org.apache.cassandra.concurrent.NamedThreadFactory;
 import org.apache.cassandra.db.marshal.AbstractType;
 import org.apache.cassandra.index.sai.IndexContext;
 import org.apache.cassandra.index.sai.disk.PostingList;
@@ -42,6 +46,7 @@ import org.apache.cassandra.index.sai.utils.NamedMemoryLimiter;
 import org.apache.cassandra.index.sai.utils.PrimaryKey;
 import org.apache.cassandra.index.sai.utils.TypeUtil;
 import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.ExecutorUtils;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.BytesRefBuilder;
 import org.apache.lucene.util.StringHelper;
@@ -173,11 +178,17 @@ public abstract class SegmentBuilder
     public static class VectorSegmentBuilder extends SegmentBuilder
     {
         private final CassandraOnHeapHnsw<Integer> graphIndex;
+        private final DebuggableThreadPoolExecutor tp;
 
         public VectorSegmentBuilder(AbstractType<?> termComparator, NamedMemoryLimiter limiter, IndexWriterConfig indexWriterConfig)
         {
             super(termComparator, limiter);
-            graphIndex = new CassandraOnHeapHnsw<>(termComparator, indexWriterConfig, false);
+            graphIndex = new CassandraOnHeapHnsw<>(termComparator, indexWriterConfig, true);
+            tp = new DebuggableThreadPoolExecutor(Runtime.getRuntime().availableProcessors() - 1,
+                                                  Integer.MAX_VALUE,
+                                                  TimeUnit.SECONDS,
+                                                  new LinkedBlockingQueue<>(1024),
+                                                  new NamedThreadFactory("Vector-index-build"));
         }
 
         @Override
@@ -189,12 +200,24 @@ public abstract class SegmentBuilder
         @Override
         protected long addInternal(ByteBuffer term, int segmentRowId)
         {
-            return graphIndex.add(term, segmentRowId, IGNORE);
+            tp.submit(() -> graphIndex.add(term, segmentRowId, IGNORE));
+            return 32; // FIXME estimate this better
         }
 
         @Override
         protected SegmentMetadata.ComponentMetadataMap flushInternal(IndexDescriptor indexDescriptor, IndexContext indexContext) throws IOException
         {
+            // block for all in-flight graph adds to complete
+            tp.shutdown();
+            try
+            {
+                tp.awaitTermination(1, TimeUnit.DAYS);
+            }
+            catch (InterruptedException e)
+            {
+                throw new RuntimeException(e);
+            }
+
             return graphIndex.writeData(indexDescriptor, indexContext, p -> p);
         }
     }
