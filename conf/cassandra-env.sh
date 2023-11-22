fdf3bf39d2f04bc74b1d7a5e03dee1363041c91d
Jonathan Ellis <jbellis@datastax.com>
2023-11-22 08:37:10 -0600

    evaluate maxBruteForceRows after we have an accurate key count

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -138,7 +138,7 @@ USING_G1=$?
 # times. If in doubt, and if you do not particularly want to tweak, go with
 # 100 MB per physical CPU core.
 
-#MAX_HEAP_SIZE="4G"
+MAX_HEAP_SIZE="16G"
 #HEAP_NEWSIZE="800M"
 
 # Set this to control the amount of arenas per-thread in glibc
